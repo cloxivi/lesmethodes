ac49a05a41482efa4603ec64100383fab971cae3
zjanvier <72319686+zjanvier@users.noreply.github.com>
2024-04-27 21:55:30 -0400

    Update MainStudent.java

--- src/MainStudent.java
+++ src/MainStudent.java
@@ -1,92 +1,26 @@
+mport java.util.List;
+
 /**
  * @author : chloelam
  * @Description :
  * @created : 2024-04-22, Monday
  **/
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-/*public class MainStudent
-{
-    // Student class to represent individual students
-        private String name;
-        private int marks;
 
-        public void Student(String name, int marks) {
-            this.name = name;
-            this.marks = marks;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getMarks() {
-            return marks;
-        }
-
-        @Override
-        public String toString() {
-            return "Student{name='" + name + "', marks=" + marks + "}";
-        }
-    }
-
-    // StudentRecords class to manage a collection of Student objects
-    class StudentRecords {
-        private List<Student> students;
-
-        public StudentRecords() {
-            students = new ArrayList<>();
-        }
-
-        // Method to add a new student to the records
-
-
-        // Method to search for students based on a specific mark
-        public List<Student> searchStudentsByMarks(int targetMarks) {
-            return students.stream()
-                    .filter(student -> student.getMarks() == targetMarks)
-                    .collect(Collectors.toList());
-        }
-
-        // Method to display all students in the records
-        public void displayAllStudents() {
-            for (Student student : students) {
-                System.out.println(student);
-            }
-        }
-
-        // Method to display students in a specific collection
-        public void displayStudentCollection(List<Student> studentCollection) {
-            for (Student student : studentCollection) {
-                System.out.println(student);
-            }
-        }
-    }
-*/
     // Main class to demonstrate adding and searching for students
-    public class Mainstudent {
+public class Mainstudent {
         public static void main(String[] args) {
-           // StudentRecords records = new StudentRecords();
 StudentManager studentmanager=new StudentManager();
-studentmanager.addstudent(new Student(434,"gdgdg","rerer",34));
-            // Adding new students to the records
+studentmanager.addstudent(new Student(434,"gdgdg","rerer",84));
+studentmanager.addstudent(new Student(44,"gdgdg","ooer",39));
+studentmanager.addstudent(new Student(4,"gutgdg","rerhhr",39));
+studentmanager.addstudent(new Student(444,"gdhfhg","reruu",79));
 
-           // records.addStudent("Bob", 72);
-           // records.addStudent("Chloe", 45);
-            //records.addStudent("Jack", 90);
-           // records.addStudent("Alma", 65);
-           // records.addStudent("Maya", 30);
 
             // Display all students
             System.out.println("All students:");
             studentmanager.displayAllStudents();
+            // ajouter les autres methodes
 
-            // Search for students with a specific mark
-            int targetMarks = 72;
-            System.out.println("\nStudents with " + targetMarks + " marks:");
-            List<Student> foundStudents = studentmanager.searchStudentsByMarks(targetMarks);
-            //studentmanager.displayStudentCollection(foundStudents);
         }
     }
 
